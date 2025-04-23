599656eab941f7767fa5ad9bbf7220d32e114353
belizsoyak <beliz.soyak@gmail.com>
2025-04-23 11:30:30 -0700

    add these too

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,17 @@ import './index.css';
 import App from './App.jsx';
 import CitiesPage from './components/CitiesPage';
 import Layout from './components/Layout';
-imp
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/cities" element={<CitiesPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  </StrictMode>
+);
